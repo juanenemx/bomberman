06b821cf0f3f507a45485ce344552078991a11fa
juanenemx <160082800+juanenemx@users.noreply.github.com>
2024-04-12 01:58:16 +0000

    Version 1

---
+++ src/tui_test.cpp
@@ -0,0 +1,25 @@
+#include <ftxui/component/animation.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <iostream>
+#include <string>
+using namespace std;
+using namespace ftxui;
+
+int main(int argc, char const *argv[])
+{
+    const string texto = "Hola mundo";
+    Element textElement = text(texto);
+    Element dibujo = hbox(textElement | border);
+
+
+    Dimensions Alto = Dimension::Fixed(10);
+    Dimensions Ancho = Dimension::Fixed(10);
+    Screen pantalla = Screen::Create(Ancho,Alto);
+
+    Render(pantalla,dibujo);
+    pantalla.Print();
+    cout<<endl;
+
+    return 0;
+}
