99d08f2bddd6044699742c321051874d4cc07551
juanenemx <160082800+juanenemx@users.noreply.github.com>
2024-04-19 02:00:24 +0000

    Version 2

--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -1,25 +1,61 @@
 #include <ftxui/component/animation.hpp>
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/color.hpp>
 #include <iostream>
 #include <string>
+#include <thread>
+#include <experimental/random>
+
+
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    const string texto = "Hola mundo";
-    Element textElement = text(texto);
-    Element dibujo = hbox(textElement | border);
+    int fotograma = 0;
+    string reset;
+
+    while (true)
+    {
+        /* code */
+        int r = std::experimental::randint(0,255);
+        int g = std::experimental::randint(0,255);
+        int b = std::experimental::randint(0,255);
+
+        Element personaje = spinner(21,fotograma);
+
+        //INSTRUCCIONES PARA COLORES ESPECIFICOS
+        //Decorator colorFondo = bgcolor(Color::Blue1);
+        //Decorator colorTexto = color(Color::Red1);*/
+
+        //ISNTRUCCIONES PARA COLORES RANDOMIZADOS RGB
+        Decorator colorFondo = bgcolor(Color::RGB(r,g,b));
+        Decorator colorTexto = color(Color::RGB(b,r,g));
+
+        
+        Element dibujo = border({
+            hbox(personaje) | colorFondo 
+        })| colorTexto ;
 
 
-    Dimensions Alto = Dimension::Fixed(10);
-    Dimensions Ancho = Dimension::Fixed(10);
-    Screen pantalla = Screen::Create(Ancho,Alto);
+        Dimensions Alto = Dimension::Fixed(10);
+        Dimensions Ancho = Dimension::Full();
 
-    Render(pantalla,dibujo);
-    pantalla.Print();
-    cout<<endl;
+        Screen pantalla = Screen::Create(Ancho,Alto);
+
+        Render(pantalla,dibujo);
+        pantalla.Print();
+        reset = pantalla.ResetPosition();
+
+        cout<<reset;
+
+        fotograma++;
+
+        this_thread::sleep_for(0.1s);
+    }
+    
+
 
     return 0;
 }
