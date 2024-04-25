2ad38eedcbb19f430532eb65e1d91c976da4a668
juanenemx <160082800+juanenemx@users.noreply.github.com>
2024-04-25 01:00:09 +0000

    Version 3

--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -6,16 +6,32 @@
 #include <string>
 #include <thread>
 #include <experimental/random>
-
+#include <list>
+#include<fstream>
 
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
+    list<string> textos;
+
+    fstream imagen;
+    imagen.open("./assets/imagen.txt");
+    
+    string linea;
+    while (getline(imagen, linea))
+    {
+        textos.push_back(linea);
+    }
+    
+    imagen.close();
+
     int fotograma = 0;
     string reset;
 
+    int posX = 0;
+    int posY = 6;
     while (true)
     {
         /* code */
@@ -39,12 +55,29 @@ int main(int argc, char const *argv[])
         })| colorTexto ;
 
 
-        Dimensions Alto = Dimension::Fixed(10);
+        Dimensions Alto = Dimension::Full();
         Dimensions Ancho = Dimension::Full();
 
         Screen pantalla = Screen::Create(Ancho,Alto);
-
+        
         Render(pantalla,dibujo);
+
+
+        int l = 0;
+        for (auto &&texto : textos)
+        {
+            int i = 0;
+
+            for (auto &&letra : texto)
+            {
+                pantalla.PixelAt(posX + i, posY + l).character = letra;
+                i++;
+            }
+            l++;
+        }
+        posX++;
+
+
         pantalla.Print();
         reset = pantalla.ResetPosition();
 
